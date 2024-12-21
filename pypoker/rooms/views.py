9c8f827dad86a11d708b8bd44ca66485ee77f5ec
IvanOsipov1 <ivan2015.osip@gmail.com>
2024-12-21 13:31:17 +0700

    3 players

--- pypoker/rooms/views.py
+++ pypoker/rooms/views.py
@@ -9,10 +9,13 @@ RoomPlayer = apps.get_model('rooms', 'RoomPlayer')
 def room_detail(request, unique_id):
     room = get_object_or_404(Room, unique_id=unique_id)
     players = RoomPlayer.objects.filter(room=room)
-    seats = range(1, 3)  # допустим, у вас есть количество мест
-    return render(request, 'rooms/room_2_players.html', {
+    seats = range(1, room.max_players + 1)  # Список мест от 1 до max_players
+
+    # Формируем имя шаблона на основе количества игроков
+    template_name = f'rooms/room_{room.max_players}_players.html'
+
+    return render(request, template_name, {
         'room': room,
         'players': players,
-        'seats': seats
+        'seats': seats,
     })
-
